1f61d954032a801fcc1f1a7b896e0d076d9f9313
Amit <amitrjain11@gmail.com>
2022-01-30 10:59:55 -0800

    reordering flow

---
+++ build/CMakeFiles/benchmark-blas.dir/dgemm-blas.c.o.d
@@ -0,0 +1,5 @@
+CMakeFiles/benchmark-blas.dir/dgemm-blas.c.o: \
+ /global/homes/a/amitjain/HW1_CS267_2022/dgemm-blas.c \
+ /usr/include/stdc-predef.h \
+ /opt/cray/pe/libsci/19.06.1/GNU/8.1/x86_64/include/cblas.h \
+ /opt/gcc/8.3.0/snos/lib/gcc/x86_64-suse-linux/8.3.0/include/stddef.h
---
+++ build/CMakeFiles/benchmark-naive.dir/dgemm-naive.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/benchmark-naive.dir/dgemm-naive.c.o: \
+ /global/homes/a/amitjain/HW1_CS267_2022/dgemm-naive.c \
+ /usr/include/stdc-predef.h
